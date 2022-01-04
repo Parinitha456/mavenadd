014c2c6f683996cdaacfc9da5237adcc35e9769c
Parinitha456 <96162601+Parinitha456@users.noreply.github.com>
2022-01-04 12:25:35 +0530

    Update AddNumbers.java

--- src/main/java/com/devhandson/AddNumbers.java
+++ src/main/java/com/devhandson/AddNumbers.java
@@ -2,6 +2,7 @@ package com.devhandson;
 
 public class AddNumbers {
 	
+	int a=20;
 	//method to add 2 numbers
 		public int addNumbers(int numberOne,int numberTwo) {
 			return numberOne+numberTwo;
